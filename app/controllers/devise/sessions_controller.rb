5dd4733f8673bf94d70ec9097df1a597da2536c7
Megha Gulati <meghagulati30@gmail.com>
2013-10-08 16:09:47 -0700

    session handling json

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -12,19 +12,18 @@ class Devise::SessionsController < DeviseController
 
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
-    sign_in(resource_name, resource)
-
     respond_to do |format|
         format.html { 
+          self.resource = warden.authenticate!(auth_options)
+          set_flash_message(:notice, :signed_in) if is_navigational_format?
+          sign_in(resource_name, resource)
           respond_with resource, :location => after_sign_in_path_for(resource)
         }
         format.json {
-
-          
+          render :json => {:status => 201, :success=> true }
         }
     end
+    
   end
 
   # DELETE /resource/sign_out
@@ -58,3 +57,4 @@ class Devise::SessionsController < DeviseController
     { :scope => resource_name, :recall => "#{controller_path}#new" }
   end
 end
+
